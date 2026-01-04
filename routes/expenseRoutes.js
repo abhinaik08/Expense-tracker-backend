a542c83e70177ad10eef87eedcb6fa687c9e1b76
unknown <abhinaik1947@gmail.com>
2026-01-04 12:46:49 +0530

    first commit

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { addExpense, deleteExpense, getExpense, getSingleExpense, updateExpense } from "../controllers/expenseControllers.js";
 const router = express.Router();
 router.post("/insert", addExpense)
-router.get("/view/:all", getExpense)
+router.get("/view-all", getExpense)
 router.get("/singleview/:id", getSingleExpense)
 router.put("/edit/:id", updateExpense)
 router.delete("/delete/:id", deleteExpense)
